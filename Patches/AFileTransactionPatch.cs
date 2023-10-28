cabf4e92ef377c8093cd4aac3cc98370d143730c
Nahuel Rocchetti <Nahuel3d@gmail.com>
2023-10-28 02:31:10 -0300

    Added option to disable cloud saving, getting rid of disk space errors.

---
+++ Patches/AFileTransactionPatch.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using Reptile;
+
+namespace PhotoStorage.Patches
+{
+    [HarmonyPatch(typeof(AFileTransaction))]
+    internal static class AFileTransactionPatch
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(nameof(AFileTransaction.IsLocal), MethodType.Getter)]
+        private static bool IsLocal_Prefix(ref bool __result)
+        {
+            if (PhotoStoragePlugin.DisableCloud)
+            {
+                __result = true;
+                return false;
+            }
+            return true;
+        }
+    }
+}
--- PhotoStoragePlugin.cs
+++ PhotoStoragePlugin.cs
@@ -8,11 +8,13 @@ namespace PhotoStorage
     [BepInPlugin(GUID, Name, Version)]
     internal class PhotoStoragePlugin : BaseUnityPlugin
     {
+        public static bool DisableCloud => _disableCloud.Value;
         public static int MaxPhotos => _maxPhotos.Value;
         private static ConfigEntry<int> _maxPhotos;
+        private static ConfigEntry<bool> _disableCloud;
         private const string GUID = "com.LazyDuchess.BRC.PhotoStorage";
         private const string Name = "PhotoStorage";
-        private const string Version = "1.0.0";
+        private const string Version = "1.1.0";
         private void Awake()
         {
             try
@@ -30,6 +32,12 @@ namespace PhotoStorage
 
         private void Configure()
         {
+            _disableCloud = Config.Bind("General",
+                "DisableCloud",
+                false,
+                "Disables Cloud saving/loading. Game data will be saved to and loaded from AppData/LocalLow/Team Reptile/Bomb Rush Cyberfunk, under the subfolders /SaveData/ and /Photos/ for the phone camera photos. This can help circumvent disk space errors that happen due to the low Steam Cloud storage size the game has."
+                );
+
             _maxPhotos = Config.Bind("General",
                 "MaxPhotos",
                 1000,
